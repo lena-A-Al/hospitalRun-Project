1df9bb8309c0879b85902f872dddfc0462eadbd1
lena <lenaal@lenas-MacBook-Pro.local>
2024-11-05 11:58:53 -0600

    working on linking apolloserver

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,23 @@
-const age: number = 36999;
-console.log(age);
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+// create our server:
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then((url) => {
+  console.log(`Server ready on ${url}`);
+});
